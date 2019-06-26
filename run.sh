78938b31ea88233f7196e53587a2484db528a100
Silvana Trindade <syletri@gmail.com>
2019-06-26 15:06:57 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SUMO_HOME="/home/silvana/Downloads/sumo-1_2_0"
-export PATH="/home/silvana/Downloads/sumo-1_2_0/bin:$PATH"
+export SUMO_HOME="/home/sumo-1_2_0"
+export PATH="/home/sumo-1_2_0/bin:$PATH"
 
 python runner.py
